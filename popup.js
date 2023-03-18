1c9efbbd6d22dbaba768801a46005a04dc48cd85
09Hiroshi <h.makino.0921@gmail.com>
2023-03-19 04:12:27 +0900

    ポップアップ表示時にローカルストレージからデータを読み込むようにした

--- popup.js
+++ popup.js
@@ -1,27 +1,32 @@
-const beforeInput = document.getElementById('before');
-const afterInput = document.getElementById('after');
-const fixedBefore = document.getElementById('fixed-before');
-const fixedAfter = document.getElementById('fixed-after');
-const okButton = document.getElementById('ok-button');
-
-okButton.addEventListener('click', () => {
-
-  const before = beforeInput.value
-  const after = afterInput.value
+window.addEventListener('load', () => {
   
-  fixedBefore.innerText = before
-  fixedAfter.innerText = after
-  
-  save(before, after)
-
-  before.value = ''
-  after.value = ''
-});
-
-// 設定を保存する
-const save = (before, after) => {
-  chrome.storage.local.set({ 
-    before: before,
-    after: after
+  // 設定を読み込む
+  chrome.storage.local.get(['before', 'after'], (result) => {
+    document.getElementById('fixed-before').textContent = result.before
+    document.getElementById('fixed-after').textContent = result.after
   })
-}
+
+  const beforeInput = document.getElementById('before')
+  const afterInput = document.getElementById('after')
+  const fixedBefore = document.getElementById('fixed-before')
+  const fixedAfter = document.getElementById('fixed-after')
+  const okButton = document.getElementById('ok-button')
+
+  okButton.addEventListener('click', () => {
+
+    const before = beforeInput.value
+    const after = afterInput.value
+    
+    fixedBefore.innerText = before
+    fixedAfter.innerText = after
+
+    before.value = ''
+    after.value = ''
+
+    // 設定を保存する
+    chrome.storage.local.set({ 
+      before: before,
+      after: after
+    })
+  });
+})
