5265f6fd1d0a6109b720df4775b36e25739f8bd6
09Hiroshi <h.makino.0921@gmail.com>
2023-03-19 00:48:57 +0900

    核となるHTMLと簡単なjsを作成

---
+++ popup.js
@@ -0,0 +1,19 @@
+const targetString = document.getElementById('target-string');
+const replaceString = document.getElementById('replace-string');
+const fixedTargetString = document.getElementById('fixed-target-string');
+const fixedReplaceString = document.getElementById('fixed-replace-string');
+const okButton = document.getElementById('ok-button');
+
+okButton.addEventListener('click', () => {
+
+  console.log("aaaaa")
+
+  const target = targetString.value;
+  const replace = replaceString.value;
+  
+  fixedTargetString.innerText = target;
+  fixedReplaceString.innerText = replace;
+  
+  targetString.value = '';
+  replaceString.value = '';
+});
