dbc1ee3e1158e06a616d78fb4a61c68b84bb3e03
09Hiroshi <h.makino.0921@gmail.com>
2023-03-20 23:57:02 +0900

    ボタンを押下時にURLが変換されるようにした

--- popup.js
+++ popup.js
@@ -1,32 +1,50 @@
-window.addEventListener('load', () => {
+const beforeInput = document.getElementById('before')
+const afterInput = document.getElementById('after')
+const fixedBefore = document.getElementById('fixed-before')
+const fixedAfter = document.getElementById('fixed-after')
+const okButton = document.getElementById('ok-button')
+const replaceButton = document.getElementById('replace-button')
+
+// 設定を読み込む
+chrome.storage.local.get(['before', 'after'], (result) => {
+
+  document.getElementById('fixed-before').textContent = result.before
+  document.getElementById('fixed-after').textContent = result.after
+})
+
+okButton.addEventListener('click', () => {
+
+  const before = beforeInput.value
+  const after = afterInput.value
   
-  // 設定を読み込む
-  chrome.storage.local.get(['before', 'after'], (result) => {
-    document.getElementById('fixed-before').textContent = result.before
-    document.getElementById('fixed-after').textContent = result.after
+  fixedBefore.innerText = before
+  fixedAfter.innerText = after
+
+  before.value = ''
+  after.value = ''
+
+  // 設定を保存する
+  chrome.storage.local.set({ 
+    before: before,
+    after: after
   })
+})
 
-  const beforeInput = document.getElementById('before')
-  const afterInput = document.getElementById('after')
-  const fixedBefore = document.getElementById('fixed-before')
-  const fixedAfter = document.getElementById('fixed-after')
-  const okButton = document.getElementById('ok-button')
+replaceButton.addEventListener('click', () => {
 
-  okButton.addEventListener('click', () => {
+  // 現在のタブに対してのアクション
+  chrome.tabs.query({
+    'active': true,
+    'lastFocusedWindow': true
+  }, (tabs) => {
 
-    const before = beforeInput.value
-    const after = afterInput.value
-    
-    fixedBefore.innerText = before
-    fixedAfter.innerText = after
+    // URLを変換する
+    const url = tabs[0].url
+    const newUrl = url.replace(fixedBefore.textContent, fixedAfter.textContent)
 
-    before.value = ''
-    after.value = ''
-
-    // 設定を保存する
-    chrome.storage.local.set({ 
-      before: before,
-      after: after
+    // 変換後のURLを指定する
+    chrome.tabs.update({
+      url: newUrl
     })
-  });
+  })
 })
